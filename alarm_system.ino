2b9903940e9f2e5d4d9fc7301f05c7f902c72516
Rohit Chakravarti <142516483+RohitIGEC@users.noreply.github.com>
2026-05-14 13:16:13 +0530

    Create alarm_system.ino

---
+++ alarm_system.ino
@@ -0,0 +1,15 @@
+# Temperature Alarm System
+
+## How it Works
+- Temperature > 30°C → LED ON + Buzzer ON
+- Temperature < 30°C → LED OFF + Buzzer OFF
+
+## Components
+- Arduino Uno
+- TMP36 Sensor
+- LED + Resistor
+- Piezo Buzzer
+
+## Author
+Rohit Chakravarti
+ECE Graduate 2024
